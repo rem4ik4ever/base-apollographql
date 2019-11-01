e99c8145f2de7197e245e6298915c61f0c07e881
Rem Kim <kim.rmi.93@gmail.com>
2019-10-31 20:29:08 -0400

    Authorization added

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import { buildSchema } from "type-graphql";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { RegisterResolver } from "./modules/user/Register";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { redis } from "./redis";
+import cors from "cors";
+import { LoginResolver } from "./modules/user/Login";
+import { MeResolver } from "./modules/user/Me";
 
 // import mongoose from "mongoose";
 // import resolvers from "./resolvers";
@@ -17,7 +23,13 @@ const startServer = async () => {
   const app = express();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver]
+    resolvers: [RegisterResolver, LoginResolver, MeResolver],
+    authChecker: ({ context: { req } }) => {
+      if (req.session.userId) {
+        return true;
+      }
+      return false;
+    }
   });
 
   // const schema = makeExecutableSchema({
@@ -26,8 +38,35 @@ const startServer = async () => {
   // });
 
   const server = new ApolloServer({
-    schema
+    schema,
+    context: ({ req }: any) => ({ req })
   });
+
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000"
+    })
+  );
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis as any
+      }),
+      name: "qid",
+      secret: "some-secret-from-env",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365
+      }
+    })
+  );
 
   server.applyMiddleware({ app });
 
